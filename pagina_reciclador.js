0102c4c537c2a267636df1f66e1e7e62160e458f
lean2025-prog <haise7505@gmail.com>
2025-10-08 13:53:40 -0300

    Alterações na página do reciclador

---
+++ pagina_reciclador.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Pega o elemento h1 onde a mensagem vai aparecer
+    const frase_cidadao = document.querySelector(".mensagem_de_parabens_cidadao");
+
+    // Pega o nome do usuário que foi salvo no LocalStorage
+    const nome_reciclador = localStorage.setItem("nome_cidadao");
+
+    // Atualiza o texto do h1
+    frase_cidadao.innerText = `Olá ${nome}! Parabéns pela iniciativa em contribuir com a seleção consciente do seu lixo.`;
+
+     const botao = document.getElementById('botao_inicio_cidadao');
+    botao.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    })})
